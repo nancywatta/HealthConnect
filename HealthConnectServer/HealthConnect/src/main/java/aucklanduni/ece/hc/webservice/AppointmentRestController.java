56947a3f457a92cb9c2443462e908ce82b8ad9e0
ChengchenLyu <clyu843@aucklanduni.ac.nz>
2014-09-13 12:45:07 +1200

    add show appointment in AppointmentRestController

--- HealthConnectServer/HealthConnect/src/main/java/aucklanduni/ece/hc/webservice/AppointmentRestController.java
+++ HealthConnectServer/HealthConnect/src/main/java/aucklanduni/ece/hc/webservice/AppointmentRestController.java
@@ -2,7 +2,9 @@ package aucklanduni.ece.hc.webservice;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import aucklanduni.ece.hc.repository.model.Account;
 import aucklanduni.ece.hc.repository.model.Appointment;
 import aucklanduni.ece.hc.repository.model.Dictionary;
+import aucklanduni.ece.hc.repository.model.Group;
 import aucklanduni.ece.hc.service.AccountService;
 import aucklanduni.ece.hc.service.AppointmentService;
 import aucklanduni.ece.hc.service.DictionaryService;
@@ -78,4 +81,63 @@ public class AppointmentRestController {
 
 		return message;
 	}
+
+
+
+/*
+ * /**
+	 * 
+	 * @Title: showAppointments 
+	 * @Description: Service will return all the appointments of the given
+	 * accountId.
+	 *  
+	 * @param request
+	 * @param response
+	 * @param accountId 
+	 * @return HCMessage
+	 * @throws
+	 */
+	@RequestMapping(value="/showAppointments",method = RequestMethod.GET
+			,headers="Accept=application/json"
+			)
+	public HCMessage showAppointments(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("accountId") long accountId){
+		HCMessage message = new  HCMessage();
+		List<Appointment> appointmentList = new ArrayList<Appointment>();
+		Map<String, ArrayList<Appointment>> appointmentArray = new HashMap<String, ArrayList<Appointment>>();
+		try {
+			Account account = null;
+			// check if given accountId exists
+			account = accountService.findById(accountId);
+			if(account == null) {
+				throw new ValidationFailException("Account does not exist");
+			}
+
+			appointmentList = appointmentService.findByHql("select distinct g from appointment g, "
+					+ "inner join app_acc_ref aar "
+					+ "on "
+					+ "g.id = arr.appointment_id"
+					+ "inner join account ac"
+					+ "on"
+					+ "ac.id=aar.account_id "
+					+ "and aar.account_id= " + accountId);
+			
+
+			// if no appointment exists for the given account
+			if(appointmentList == null || appointmentList.size() < 1) 
+				throw new ValidationFailException("No Group Exists for given Account");
+
+			appointmentArray.put("groups", (ArrayList<Appointment>)appointmentList);
+			message.setSuccess(appointmentArray);
+
+		}catch(ValidationFailException ve) {
+			message.setFail("404", ve.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			message.setFail("400", e.getMessage());
+		}
+		return message;
+	
+}
+
 }
