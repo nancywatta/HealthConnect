60cc004539ff7f97c6a4b18f14700e31f7c6b5a2
nancywatta <nancy.watta@gmail.com>
2014-09-16 11:44:31 +1200

    JUnit Test Case for Invite User for the Group Web Service

--- HealthConnectServer/HealthConnect/src/test/java/aucklanduni/ece/hc/webservice/GroupMockTests.java
+++ HealthConnectServer/HealthConnect/src/test/java/aucklanduni/ece/hc/webservice/GroupMockTests.java
@@ -1,6 +1,7 @@
 package aucklanduni.ece.hc.webservice;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -64,7 +65,7 @@ public class GroupMockTests extends BaseContextControllerTests {
 	}
 	
 	/**
-	 * test showGroup for account that does not any 
+	 * test showGroup for account that does not have any 
 	 * group created yet  
 	 */
 	@Test  
@@ -111,6 +112,163 @@ public class GroupMockTests extends BaseContextControllerTests {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.status").value("404"))
 				.andExpect(jsonPath("$.error").value("GroupId does not exist"));
+	}	
+	
+	/**
+	 * test inviteUser for invalid accountId
+	 */
+	@Test  
+	public void inviteUserInvalidAccount() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "199")
+				.param("groupId", "199")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "1")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Account does not exist"));
+	}	
+	
+	/**
+	 * test inviteUser for invalid groupId
+	 */
+	@Test  
+	public void inviteUserInvalidGroup() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("groupId", "199")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "1")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Invalid Input"));
+	}	
+
+	/**
+	 * test inviteUser by accountId which does not
+	 * own the given groupId
+	 */
+	@Test  
+	public void inviteUserUnknownGroup() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("groupId", "34")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "1")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Invalid Input"));
+	}
+	
+	/**
+	 * test inviteUser by inviting patient when already
+	 * one patient exist in the given group
+	 */
+	@Test  
+	public void invitePatientAgain() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("groupId", "1")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "1")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Only one patient allowed per group"));
+	}	
+
+	/**
+	 * test inviteUser when nurse invites
+	 * support member in the given group
+	 */
+	@Test  
+	public void nurseInviteSM() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "2")
+				.param("groupId", "1")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "3")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Nurse can Invite only Patient to Group"));
+	}	
+
+	/**
+	 * test inviteUser when support member invites
+	 * anyone in the given group
+	 */
+	@Test  
+	public void sMInviteUser() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "3")
+				.param("groupId", "1")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "3")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Support Member cannot Invite Member to Group"));
+	}	
+
+	/**
+	 * test inviteUser when user invites
+	 * already existing member of the group again
+	 * in the same group
+	 */
+	@Test  
+	public void inviteMemberAgain() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("groupId", "1")
+				.param("emailId", "nancy.watta@gmail.com")
+				.param("roleId", "3")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("nancy.watta@gmail.com is already a member in this Group"));
+	}	
+
+	/**
+	 * test inviteUser with correct inputs
+	 */
+	@Test  
+	public void inviteUserCorrect() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/inviteUser")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("groupId", "1")
+				.param("emailId", "nancywattaa@gmail.com")
+				.param("roleId", "3")
+				)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value("200"));
 	}	
 
 }
