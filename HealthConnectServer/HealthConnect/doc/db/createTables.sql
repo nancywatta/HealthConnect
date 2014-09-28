34e6812f51bfb25fc29fd73213dc235c56b9cf87
nancywatta <nancy.watta@gmail.com>
2014-09-28 13:03:24 +1300

    Update Database with Audit Tables. Avoid Auto creation of database through Hibernate, since it does not add foreign Key Constraint.

--- HealthConnectServer/HealthConnect/doc/db/createTables.sql
+++ HealthConnectServer/HealthConnect/doc/db/createTables.sql
@@ -1,5 +1,11 @@
 USE hc;
 
+CREATE TABLE `REVINFO` (
+  `REV` int(11) NOT NULL AUTO_INCREMENT,
+  `REVTSTMP` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`REV`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
 CREATE TABLE `ACCOUNT` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `email` varchar(64) NOT NULL,
@@ -14,6 +20,22 @@ CREATE TABLE `ACCOUNT` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 ;
 
+CREATE TABLE `ACCOUNT_AUD` (
+  `ID` bigint(20) NOT NULL,
+  `REV` int(11) NOT NULL,
+  `REVTYPE` tinyint(4) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `last_login_date` datetime DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`,`REV`),
+  KEY `FK_pbs9xhenk5lq31uiinsgkc1t6` (`REV`),
+  CONSTRAINT `FK_pbs9xhenk5lq31uiinsgkc1t6` FOREIGN KEY (`REV`) REFERENCES `REVINFO` (`REV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `GROUP_INFO` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `groupname` varchar(64) NOT NULL,
@@ -23,6 +45,19 @@ CREATE TABLE `GROUP_INFO` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 ;
+
+CREATE TABLE `GROUP_INFO_AUD` (
+  `ID` bigint(20) NOT NULL,
+  `REV` int(11) NOT NULL,
+  `REVTYPE` tinyint(4) DEFAULT NULL,
+  `groupname` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`,`REV`),
+  KEY `FK_3ckdh12knuv98vmwtkjnftchj` (`REV`),
+  CONSTRAINT `FK_3ckdh12knuv98vmwtkjnftchj` FOREIGN KEY (`REV`) REFERENCES `REVINFO` (`REV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `MEMBER` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -40,6 +75,25 @@ CREATE TABLE `MEMBER` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 ;
 
+CREATE TABLE `MEMBER_AUD` (
+  `ID` bigint(20) NOT NULL,
+  `REV` int(11) NOT NULL,
+  `REVTYPE` tinyint(4) DEFAULT NULL,
+  `account_id` bigint(20) DEFAULT NULL,
+  `group_id` bigint(20) DEFAULT NULL,
+  `role_id` bigint(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  `isActive` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`,`REV`),
+  KEY `FK_l987374lkixp1qutcmck072ee` (`REV`),
+  KEY `mem_aud_acc_fk` (`account_id`),
+  KEY `mem_aud_group_fk` (`group_id`),
+  CONSTRAINT `mem_aud_group_fk` FOREIGN KEY (`group_id`) REFERENCES `group_info` (`id`),
+  CONSTRAINT `mem_aud_acc_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`),
+  CONSTRAINT `FK_l987374lkixp1qutcmck072ee` FOREIGN KEY (`REV`) REFERENCES `REVINFO` (`REV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `APPOINTMENT` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
@@ -52,11 +106,30 @@ CREATE TABLE `APPOINTMENT` (
   `created_date` datetime DEFAULT NULL,
   `updated_date` datetime DEFAULT NULL,
   `expiration_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
   KEY `app_group_fk` (`group_id`),
-  CONSTRAINT `app_group_fk` FOREIGN KEY (`group_id`) REFERENCES `group_info` (`id`),
+  CONSTRAINT `app_group_fk` FOREIGN KEY (`group_id`) REFERENCES `group_info` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 ; 
+
+CREATE TABLE `APPOINTMENT_AUD` (
+  `ID` bigint(20) NOT NULL,
+  `REV` int(11) NOT NULL,
+  `REVTYPE` tinyint(4) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `desciption` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `isShared` varchar(255) DEFAULT NULL,
+  `group_id` bigint(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`,`REV`),
+  KEY `FK_quebkn23e9pvwin25f8j8mig2` (`REV`),
+  CONSTRAINT `FK_quebkn23e9pvwin25f8j8mig2` FOREIGN KEY (`REV`) REFERENCES `REVINFO` (`REV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `APP_ACC_REF` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -70,6 +143,18 @@ CREATE TABLE `APP_ACC_REF` (
   CONSTRAINT `appaccref_acc_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 ;
+
+CREATE TABLE `APP_ACC_REF_AUD` (
+  `ID` bigint(20) NOT NULL,
+  `REV` int(11) NOT NULL,
+  `REVTYPE` tinyint(4) DEFAULT NULL,
+  `appointment_id` bigint(20) DEFAULT NULL,
+  `account_id` bigint(20) DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`,`REV`),
+  KEY `FK_8ydb8pqmq863gvfewm3ldf49k` (`REV`),
+  CONSTRAINT `FK_8ydb8pqmq863gvfewm3ldf49k` FOREIGN KEY (`REV`) REFERENCES `REVINFO` (`REV`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `TASK` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
