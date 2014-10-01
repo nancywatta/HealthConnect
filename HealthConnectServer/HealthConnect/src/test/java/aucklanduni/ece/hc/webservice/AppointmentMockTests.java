789062223dfa8d409bce7d3a96be948d1f6bb9c3
LiWhitney <pli552@aucklanduni.ac.nz>
2014-10-01 23:05:14 +1300

    Junit Tests for appointments

--- HealthConnectServer/HealthConnect/src/test/java/aucklanduni/ece/hc/webservice/AppointmentMockTests.java
+++ HealthConnectServer/HealthConnect/src/test/java/aucklanduni/ece/hc/webservice/AppointmentMockTests.java
@@ -83,77 +83,6 @@ public class AppointmentMockTests extends BaseContextControllerTests {
 				.andExpect(jsonPath("$.error").value("No Appointment Exists for given Account"));
 	}	
 
-	//Junit test for view appointments by group
-	 /**
-		 * test viewAppointment for valid account 
-		 */
-		@Test  
-		public void viewAppointmentByGroupForValidAccount() throws Exception {  
-
-			this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
-					.contentType(MediaType.APPLICATION_JSON)
-					.param("accountId", "1")
-					.param("groupId", "1")
-					)
-					.andDo(print())
-					.andExpect(status().isOk())
-					.andExpect(jsonPath("$.status").value("200"))
-					.andExpect(jsonPath("$.response.appointments").isArray())
-					.andExpect(jsonPath("$.response.appointments[0].appointmentname").value("haha"));
-		}
-
-		/**
-		 * test appointment for invalid account 
-		 */
-		@Test  
-		public void viewAppointmentbyGroupForInvalidAccount() throws Exception {  
-
-			this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
-					.contentType(MediaType.APPLICATION_JSON)
-					.param("accountId", "199")
-					.param("groupId", "1")
-					)
-					.andDo(print())
-					.andExpect(status().isOk())
-					.andExpect(jsonPath("$.status").value("404"))
-					.andExpect(jsonPath("$.error").value("Account does not exist"));
-		}
-		
-		/**
-		 * test appointment for invalid account 
-		 */
-		@Test  
-		public void viewAppointmentbyGroupForInvalidGroup() throws Exception {  
-
-			this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
-					.contentType(MediaType.APPLICATION_JSON)
-					.param("accountId", "1")
-					.param("groupId", "199")
-					)
-					.andDo(print())
-					.andExpect(status().isOk())
-					.andExpect(jsonPath("$.status").value("404"))
-					.andExpect(jsonPath("$.error").value("Group does not exist"));
-		}
-		
-		
-		/**
-		 * test viewAppointment for account that does not have any 
-		 * appointment created yet  
-		 */
-		@Test  
-		public void emptyAppointmentByGroup() throws Exception {  
-
-			this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
-					.contentType(MediaType.APPLICATION_JSON)
-					.param("accountId", "3")
-					)
-					.andDo(print())
-					.andExpect(status().isOk())
-					.andExpect(jsonPath("$.status").value("404"))
-					.andExpect(jsonPath("$.error").value("No Appointment Exists for given Account"));
-		}	
-
 	
 	/**
 	 * test createAppointment with correct inputs
@@ -164,7 +93,7 @@ public class AppointmentMockTests extends BaseContextControllerTests {
 		this.mockMvc.perform(post(URL+"/createAppointment")
 				.contentType(MediaType.APPLICATION_JSON)
 				.param("accountId", "1")
-				.param("roleId", "0")
+				.param("groupId", "1")
 				.param("appointmentName", "withJam")
 				.param("appointmentLocation", "University")
 				)
@@ -177,17 +106,18 @@ public class AppointmentMockTests extends BaseContextControllerTests {
 	 * test createAppointment with invalid inputs
 	 */
 	@Test  
-	public void addAppointmentWrongByRole() throws Exception {  
+	public void addAppointmentWrongByGroup() throws Exception {  
 
-		this.mockMvc.perform(post(URL+"/createGroup")
+		this.mockMvc.perform(post(URL+"/createAppointment")
 				.contentType(MediaType.APPLICATION_JSON)
 				.param("accountId", "1")
-				.param("roleId", "3")//only the nurse can create an appointment
+				.param("groupId", "10000")//
 				.param("appointmentName", "withLeehom")
 				.param("appointmentLocation", "Cafe")
 				)
 				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"));
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Group does not exist"));
 	}
 
 	/**
@@ -204,7 +134,8 @@ public class AppointmentMockTests extends BaseContextControllerTests {
 				.param("appointmentLocation", "Boston")
 				)
 				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"));
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Account does not exist"));
 	}
 	
 	/**
@@ -230,168 +161,254 @@ public class AppointmentMockTests extends BaseContextControllerTests {
 				.andExpect(jsonPath("$.status").value("404"));
 	}
 	
+	/**
+	 * test updateAppointment with invalid inputs
+	 */
+	@Test  
+	public void updateAppointmentWrongByAppointment() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/updateAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")//not found
+				.param("groupId", "1")
+				.param("appointmentId", "10000")
+				.param("appointmentLocation", "Boston")
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Appointment does not exist"));
+	}
+	/**
+	 * test updateAppointment with invalid inputs
+	 */
+	@Test  
+	public void updateAppointmentWrongByAccount() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/updateAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")//not found
+				.param("groupId", "1")
+				.param("appointmentId", "10000")
+				.param("appointmentLocation", "Boston")
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Account does not exist"));
+	}
+	/**
+	 * test updateAppointment with invalid inputs
+	 */
+	@Test  
+	public void updateAppointmentWrongByGroup() throws Exception {  
+
+		this.mockMvc.perform(post(URL+"/updateAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")//not found
+				.param("groupId", "100000")
+				.param("appointmentId", "1")
+				.param("appointmentLocation", "Boston")
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Group does not exist"));
+	}
+	
+	
+	//Junit test for view appointments by group
+		 /**
+			 * test viewAppointment for valid account 
+			 */
+			@Test  
+			public void viewAppointmentByGroupForValidAccount() throws Exception {  
+
+				this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.param("accountId", "1")
+						.param("groupId", "1")
+						)
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.status").value("200"))
+						.andExpect(jsonPath("$.response.appointments").isArray())
+						.andExpect(jsonPath("$.response.appointments[0].appointmentname").value("haha"));
+			}
+
+			/**
+			 * test appointment for invalid account 
+			 */
+			@Test  
+			public void viewAppointmentbyGroupForInvalidAccount() throws Exception {  
+
+				this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.param("accountId", "199")
+						.param("groupId", "1")
+						)
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.status").value("404"))
+						.andExpect(jsonPath("$.error").value("Account does not exist"));
+			}
+			
+			/**
+			 * test appointment for invalid account 
+			 */
+			@Test  
+			public void viewAppointmentbyGroupForInvalidGroup() throws Exception {  
+
+				this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.param("accountId", "1")
+						.param("groupId", "199")
+						)
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.status").value("404"))
+						.andExpect(jsonPath("$.error").value("Group does not exist"));
+			}
+			
+			
+			/**
+			 * test viewAppointment for account that does not have any 
+			 * appointment created yet  
+			 */
+			@Test  
+			public void emptyAppointmentByGroup() throws Exception {  
+
+				this.mockMvc.perform(get(URL+"/viewAppointmentByGroup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.param("accountId", "3")
+						)
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.status").value("404"))
+						.andExpect(jsonPath("$.error").value("No Appointment Exists for given Account"));
+			}	
 
 	/**
-	 * test filterAppsByUsername : The groupId must exist.
+	 * test filterAppointment: The accountId must exist.
 	 */
 	@Test 
-	public void filterAppsByUsernameinvaildaccountId() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByUsername")
+	public void filterAppointmentinvaildaccountId() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
 				.contentType(MediaType.APPLICATION_JSON)
 				.param("accountId", "1000000")//this accountId is not exist
-				.param("roleId", "2")
-				.param("username", "")
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Account does not exist"));
+	}
+	/**
+	 * test filterAppointment: If input memberId, the account must have the member.
+	 */
+	@Test 
+	public void filterAppointmentinvailmemeberId() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")//this accountId is not exist
+				.param("memberId", "34")//this memberId does not belongs to the account
+				
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Member does not exist"));
+	}
+	/**
+	 * test filterAppointment: If user does not input groupId, then it  will
+	 * find all groups in which the accountId and memberId are part of.
+	 */
+	@Test 
+	public void filterAppointmentnogroups() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "3")
+				.param("memberId", "2")
+				
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Invalid Input"));
+	}
+	/**
+	 * test filterAppointment: If user does not input groupId, then it  will
+	 * find all groups in which the accountId and memberId are part of.
+	 */
+	@Test 
+	public void filterAppointmentgroupId() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("memberId", "1")
+				.param("groupId", "100000")
+				
+				)
+				.andDo(print())
+				.andExpect(jsonPath("$.status").value("404"))
+				.andExpect(jsonPath("$.error").value("Group does not exist"));
+	}
+	
+	/**
+	 * test filterAppointment: the input startDate should be suitable*/
+	@Test 
+	public void filterAppointmentinvalidSDate() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("accountId", "1")
+				.param("startDate","17536287")
+				.param("endDate", "2016-02-10")
+				
 				)
 				.andDo(print())
 				.andExpect(jsonPath("$.status").value("404"));
 	}
+   
 	/**
-	 * test filterAppsByUsername : Nurse filter appointments when he/she have a group as a nurse.
-	 */
+	 * test filterAppointment: the input endDate should be suitable*/
 	@Test 
-	public void filterAppsByUsernamenogroups() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByUsername")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "2")
-				.param("roleId", "2")
-				.param("username", "")
-				)
-				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("invalid input!"));
-	}
-	/**
-	 * test filterAppsByUsername : Only nurse can filter appointments.
-	 */
-	@Test 
-	public void filterAppsByUsernameroleId() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByUsername")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "2")
-				.param("roleId", "3")
-				.param("username", "")
-				)
-				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("invalid input!"))
-				;
-	}
-	/**
-	 * test filterAppsByUsername : Only nurse can filter appointments.
-	 */
-	@Test 
-	public void filterAppsByUserusername() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByUsername")
+	public void filterAppointmentinvalidEDate() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
 				.contentType(MediaType.APPLICATION_JSON)
 				.param("accountId", "1")
-				.param("roleId", "2")
-				.param("username", "lala")
-				)
-				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("invalid username!"))
-				;
-    }	
-	/**
-	 * test filterAppsByUsername with correct inputs
-	 */
-	@Test 
-	public void filterAppsByUsernamecorrect() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByUsername")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "1")
-				.param("roleId", "2")
-				.param("username", "nancy")
-				)
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.status").value("200"))
-				;
-    }	
-	
-	
-	
-	/**
-	 * test filterAppsByDate : The groupId must exist.
-	 */
-	@Test 
-	public void filterAppsByDateinvaildaccountId() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByDate")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "1000000")//this accountId is not exist
-				.param("roleId", "2")
-				.param("username", "")
+				.param("startDate","2014-02-10")
+				.param("endDate", "45466575")
+				
 				)
 				.andDo(print())
 				.andExpect(jsonPath("$.status").value("404"));
 	}
+	
 	/**
-	 * test filterAppsByDate : Nurse filter appointments when he/she have a group as a nurse.
-	 */
+	 * test filterAppointment: the input Date should be suitable*/
 	@Test 
-	public void filterAppsByDatenogroups() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByDate")
+	public void filterAppointmentinvalidDate() throws Exception {  
+		this.mockMvc.perform(post(URL+"/filterAppointment")
 				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "2")
-				.param("roleId", "2")
-				.param("username", "")
-				.param("startDate", "2014-02-10")
-				.param("endDate", "2016-02-10")
+				.param("accountId", "1")
+				.param("startDate","gdfjjerhrj")
+				.param("endDate", "45466575")
+				
 				)
 				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("invalid input!"))
-				;
+				.andExpect(jsonPath("$.status").value("404"));
 	}
-	/**
-	 * test filterAppsByDate : Only nurse can filter appointments.
-	 */
-	@Test 
-	public void filterAppsByDateroleId() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByDate")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "2")
-				.param("roleId", "3")
-				.param("username", "")
-				.param("startDate", "2014-02-10")
-				.param("endDate", "2016-02-10")
-				)
-				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("only nurse can do this action!"));
-	}
-	/**
-	 * test filterAppsByDate : Only nurse can filter appointments.
-	 */
-	@Test 
-	public void filterAppsByDateusername() throws Exception {  
-		this.mockMvc.perform(post(URL+"/filterAppsByDate")
-				.contentType(MediaType.APPLICATION_JSON)
-				.param("accountId", "2")
-				.param("roleId", "3")
-				.param("username", "lala")
-				.param("startDate", "2014-02-10")
-				.param("endDate", "2016-02-10")
-				)
-				.andDo(print())
-				.andExpect(jsonPath("$.status").value("404"))
-				.andExpect(jsonPath("$.error").value("invalid username!"))
-				;
-    }	
 //	/**
-//	 * test filterAppsByDate with correct inputs
+//	 * test filterAppsByDate : Nurse filter appointments when he/she have a group as a nurse.
 //	 */
 //	@Test 
-//	public void filterAppsByDatecorrect() throws Exception {  
+//	public void filterAppsByDatenogroups() throws Exception {  
 //		this.mockMvc.perform(post(URL+"/filterAppsByDate")
 //				.contentType(MediaType.APPLICATION_JSON)
-//				.param("accountId", "1")
+//				.param("accountId", "2")
 //				.param("roleId", "2")
-//				.param("username", "nancy")
+//				.param("username", "")
+//				.param("startDate", "2014-02-10")
+//				.param("endDate", "2016-02-10")
 //				)
 //				.andDo(print())
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.status").value("200"))
+//				.andExpect(jsonPath("$.status").value("404"))
+//				.andExpect(jsonPath("$.error").value("invalid input!"))
 //				;
-//    }	
+//	}
+
+
 }
